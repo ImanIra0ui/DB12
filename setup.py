51cdc13684713c2e5ab8761fb19dd71e0b4fbf67
Imane Iraoui <iraoui.imane@gmail.com>
2021-07-06 22:34:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-[metadata]
-name = DB12
-description = DIRAC Benchmark 2012
-long_description = file: README.md
-long_description_content_type = text/markdown
-url = https://github.com/pypa/DIRACGrid/DB12
-project_urls =
-    Bug Tracker = https://github.com/pypa/DIRACGrid/DB12/issues
-classifiers =
-    Programming Language :: Python :: 3
-    Programming Language :: Python :: 2.7
-    Operating System :: OS Independent
+import setuptools
 
-[options]
-package_dir =
-    = src
-packages = find:
-python_requires = >=2.7
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
-[options.packages.find]
-where = src
+setuptools.setup(
+    name="DB12",
+    description="DIRAC Benchmark 2012",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/DIRACGrid/DB12",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/DIRACGrid/DB12/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=2.7",
+)
