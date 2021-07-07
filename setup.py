74c9cc73ce7706de9fe5a70c3ee1ff96f5c928da
Imane Iraoui <iraoui.imane@gmail.com>
2021-07-07 16:21:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,6 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/DIRACGrid/DB12",
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
     project_urls={
         "Bug Tracker": "https://github.com/pypa/DIRACGrid/DB12/issues",
     },
