2ba8ca42bc4dc2b9da9ed852ac8faaeadfde1b39
Imane Iraoui <iraoui.imane@gmail.com>
2021-07-09 16:16:00 +0100

    Update __init__.py

--- src/db12/__init__.py
+++ src/db12/__init__.py
@@ -1,10 +1,2 @@
 from __future__ import absolute_import
-
-__all__ = [
-    "single_dirac_benchmark",
-    "multiple_dirac_benchmark",
-    "wholenode_dirac_benchmark",
-    "jobslot_dirac_benchmark"
-]
-
 from db12 import smth
