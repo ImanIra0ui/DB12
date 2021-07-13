7aff0f2bd8b111cce6db6a87cf1559a0c8d44043
ImanIra0ui <potatoez85@gmail.com>
2021-07-13 01:14:09 +0100

    import fix

--- src/db12/__main__.py
+++ src/db12/__main__.py
@@ -13,10 +13,7 @@ from __future__ import print_function
 from __future__ import division
 from __future__ import absolute_import
 
-import os
 import sys
-import random
-import multiprocessing
 
 from db12.benchmark import single_dirac_benchmark
 from db12.benchmark import multiple_dirac_benchmark
