31000f261fd25ca99bd8bf7a95995ded1e37a162
ImanIra0ui <potatoez85@gmail.com>
2021-07-19 15:36:53 +0100

    ;

--- src/db12/__main__.py
+++ src/db12/__main__.py
@@ -113,9 +113,9 @@ dirac_benchmark.py is distributed from  https://github.com/DIRACGrid/DB12
 
     parser = argparse.ArgumentParser()
     #pylint: disable=line-too-long
-    parser.add_argument("--iterations", type=int, help="number of iterations to perform", action='store_true')
-    parser.add_argument("--extra-iteration", type=bool, help="whether an extra iteration is needed", action='store_true')
-    parser.add_argument("--json", type=str, help="generate json files", action='store_true')
+    parser.add_argument("--iterations", type=int, help="number of iterations to perform")
+    parser.add_argument("--extra-iteration", type=bool, help="whether an extra iteration is needed")
+    parser.add_argument("--json", type=str, help="generate json files")
     parser.add_argument("copy", nargs='?', help="number of copies", const='', default='', action='store_true')
     parser.add_argument('--version', action='version', version=VERSION)
 
